d2f5ffef77afabb5d7598b5092e8468bac36b4ad
Nazira06 <nazikkydyralieva@gmail.com>
2020-10-23 12:13:37 +0600

    HT12

--- while 2/1.py
+++ while 2/1.py
@@ -1,12 +1,8 @@
-username_list = []
-password_list = []
-i = 0
-while i < 5:
-    username = input("ВВедите имя пользователя:")
-    password = input("Введите пароль:")
-    username_list.append(username)
-    password_list.append(password)
-    print(f'пользователь под номером{i} зарегистрирован')
-    i = i +1
-print(username_list)
-print(password_list)
+namder_list = [1, 20, 30, 2, 5]
+k = 4
+c = 100
+namder_list.insert(k,c)
+print(namder_list)
+
+
+
