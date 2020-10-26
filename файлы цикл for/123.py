5abc5962766c904706a20ea00c14ecb927e057ef
Nazira06 <nazikkydyralieva@gmail.com>
2020-10-26 10:58:12 +0600

    123456789

---
+++ файлы цикл for/123.py
@@ -0,0 +1,4 @@
+for number in [1, 2, 3, -1, -2]:
+    if number > 0:
+        print(number)
+    
---
+++ файлы цикл for/12345.py
@@ -0,0 +1,5 @@
+number = [1, 2, 3, 4, 5]
+for number in range(1,4):
+    for number2 in range(1, 4):
+        print(f"первое число = {number}, второе число равно = {number2}")
+
